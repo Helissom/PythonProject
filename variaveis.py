86edace4acf6a7c1d5437bd48e251b8471b821f9
helissom <helissom1994@gmail.com>
2025-11-22 09:58:06 -0300

    Adicionar arquivo variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,8 @@
+# Programa simples que solicita ao usuário seu nome, idade e peso,
+# armazena esses valores em variáveis e depois exibe as informações.
+
+nome = input('Qual é o seu nome? ')
+idade = input('Qual é a sua idade? ')
+altura = input('Qual é o seu peso? ')
+
+print(nome, idade, altura)
