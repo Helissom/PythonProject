b734247e1f95c5f79bbb4b49094bb1e1db81c1be
helissom <helissom1994@gmail.com>
2025-12-02 17:29:35 -0300

    Primeiro commit

---
+++ appfood.py
@@ -0,0 +1,10 @@
+print("""
+      𝔉𝔬𝔬𝔡 𝔄𝔫𝔡 𝔅𝔢𝔳𝔢𝔯𝔞𝔤𝔢 \n""")
+
+print('1. Cadastrar Restaurante')
+print('2. Listar produtor')
+print('3. Ativar Restaurate ')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f"Você escolheu a opção {opcao_escolhida}\n")
